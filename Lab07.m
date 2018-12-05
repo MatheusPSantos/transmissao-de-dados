230baf6c09f1169566e5bf81f3f1ea8066e33ca3
Matheus Pereira dos Santos <pereiramatheus064@gmail.com>
2018-12-05 11:14:20 -0200

    Update Lab07.m

--- Lab07.m
+++ Lab07.m
@@ -5,12 +5,13 @@ clc; clear all; close all;
 %% Atividade 1
 
 vetorSNRdB = -2:10; %vetor de Eb/No
-vetorSNRw = 10.^(vetorSNRdB/10); %convers�o para watts
+vetorSNRw = 10.^(vetorSNRdB/10); %conversão para watts
 vetorProbEr = qfunc(sqrt(2*vetorSNRw)); %vetor de probabilidade de erro
 figure
 semilogy(vetorSNRdB, vetorProbEr, 'k-');
-ylabel('Probabilidade de Erro');
-xlabel('Eb/No (dB)');
+legend('Não Codificado','Location','SouthOutside');
+ylabel('Probabilidade de Erro')
+xlabel('Eb/No (dB)')
 grid on
 hold on
 
@@ -21,12 +22,46 @@ nbits = 100000;
 
 vetEbNo = [-2:6, 6.5:0.5:9]; % Eb/No em dB
 
-for k=1 : length(vetEbNo)
-    [veterros(k)] = BipolarRRC2encode(vetEbNo(k), nbits, alpha, n, k);
+for i=1 : length(vetEbNo)
+    [veterros(i)] = BipolarRRC2encode(vetEbNo(i), nbits, alpha, n, k);
 end
+
 semilogy(vetEbNo, veterros/nbits, 'r-d');
+legend('Codificado com (7,3)', 'Location', 'SouthOutside');
 
 %% Atividade 3
+n = 15; k = 11;
+a = 100000;
+a = a-mod(a, k);
 
+for i = 1 : length(vetEbNo)
+    [veterros(i)] = BipolarRRC2encode(vetEbNo(i), a, alpha, n, k);
+end
 
+semilogy(vetEbNo, veterros/a, 'g-d');
+legend('Codificado com (15,11)', 'Location', 'SouthOutside');
+hold on
+
+%% Atividade 4
+n = 31; k = 26;
+a = 100000;
+a = a-mod(a, k);
+for i=1 : length(vetEbNo)
+    [veterros(i)] = BipolarRRC2encode(vetEbNo(i), a, alpha, n, k);
+end
+
+semilogy(vetEbNo, veterros/a, 'b-d');
+legend('Codificado com (31,26)', 'Location', 'SouthOutside');
+hold on
+
+%% Atividade 5
+n = 127; k = 120;
+a = 100000;
+a = a-mod(a, k);
+for i = 1 : length(vetEbNo)
+    [veterros(i)] = BipolarRRC2encode(vetEbNo(i), a, alpha, n, k);
+end
+
+semilogy(vetEbNo, veterros/a, 'r-d');
+legend('Codificado com (127,120)','Location','SouthOutside');
 
