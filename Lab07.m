729c6be05820cd9b688be432643a1b662fa8a640
Matheus Pereira dos Santos <pereiramatheus064@gmail.com>
2018-12-05 19:54:39 -0200

    Update Lab07.m

--- Lab07.m
+++ Lab07.m
@@ -5,11 +5,11 @@ clc; clear all; close all;
 %% Atividade 1
 
 vetorSNRdB = -2:10; %vetor de Eb/No
-vetorSNRw = 10.^(vetorSNRdB/10); %conversão para watts
+vetorSNRw = 10.^(vetorSNRdB/10); %conversÃ£o para watts
 vetorProbEr = qfunc(sqrt(2*vetorSNRw)); %vetor de probabilidade de erro
 figure
 semilogy(vetorSNRdB, vetorProbEr, 'k-');
-legend('Não Codificado','Location','SouthOutside');
+legend('NÃ£o Codificado','Location','SouthOutside');
 ylabel('Probabilidade de Erro')
 xlabel('Eb/No (dB)')
 grid on
@@ -20,7 +20,7 @@ n= 7; k = 3;
 alpha = 0.333;
 nbits = 100000;
 
-vetEbNo = [-2:6, 6.5:0.5:9]; % Eb/No em dB
+vetEbNo = [-2:6 6.5:0.5:9]; % Eb/No em dB
 
 for i=1 : length(vetEbNo)
     [veterros(i)] = BipolarRRC2encode(vetEbNo(i), nbits, alpha, n, k);
@@ -64,4 +64,63 @@ end
 
 semilogy(vetEbNo, veterros/a, 'r-d');
 legend('Codificado com (127,120)','Location','SouthOutside');
+
+%% Atividade 7
+n = 4;
+k = 7;
+alpha = 0.333;
+nbits = 100000;
+
+vetEbNo = [-2:6 6.5:0.5:9]; % Eb/No em dB
+
+for i=1 : length(vetEbNo)
+    [veterros(i)] = BipolarRRC2encode(vetEbNo(i), nbits, alpha, n, k);
+end
+figure
+
+semilogy(vetEbNo + 10*log10(k/7), veterros/nbits, 'r-d');
+legend('Codificado com (7,4)', 'Location', 'SouthOutside');
+hold on
+% ------
+n = 15;
+k = 11;
+alpha = 0.333;
+nbits = 100000;
+
+vetEbNo = [-2:6 6.5:0.5:9]; % Eb/No em dB
+
+for i=1 : length(vetEbNo)
+    [veterros(i)] = BipolarRRC2encode(vetEbNo(i), nbits, alpha, n, k);
+end
+semilogy(vetEbNo + 10*log10(k/7), veterros/nbits, 'g-d');
+legend('Codificado com (15, 11)', 'Location', 'SouthOutside');
+hold on
+% ------
+n = 31;
+k = 26;
+alpha = 0.333;
+nbits = 100000;
+
+vetEbNo = [-2:6 6.5:0.5:9]; % Eb/No em dB
+
+for i=1 : length(vetEbNo)
+    [veterros(i)] = BipolarRRC2encode(vetEbNo(i), nbits, alpha, n, k);
+end
+semilogy(vetEbNo + 10*log10(k/7), veterros/nbits, 'b-d');
+legend('Codificado com (31, 26)', 'Location', 'SouthOutside');
+hold on
+% ------
+n = 127;
+k = 120;
+alpha = 0.333;
+nbits = 100000;
+
+vetEbNo = [-2:6 6.5:0.5:9]; % Eb/No em dB
+
+for i=1 : length(vetEbNo)
+    [veterros(i)] = BipolarRRC2encode(vetEbNo(i), nbits, alpha, n, k);
+end
+semilogy(vetEbNo + 10*log10(k/7), veterros/nbits, 'r-d');
+legend('Codificado com (127,120)', 'Location', 'SouthOutside');
+hold on
 
